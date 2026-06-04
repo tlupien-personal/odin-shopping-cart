c57546c89f70651d3247e0e2a16d94564ff711bc
Tim Lupien <timlupien@gmail.com>
2026-06-04 18:02:56 -0500

    Put form on product cards part 1
    Puts the form with a quantity input and custom +/- buttons (as directed)
    but doesn't implement actual adding to cart yet.

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,12 +1,68 @@
+import { useState } from "react";
 import styles from "./ProductCard.module.css";
 
 export default function ProductCard({ data }) {
+  const [quantity, setQuantity] = useState(1);
+
+  const increment = (e) => {
+    e.preventDefault();
+    setQuantity(quantity + 1);
+  };
+
+  const decrement = (e) => {
+    e.preventDefault();
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const addToCart = (e) => {
+    e.preventDefault();
+    console.log("not implemented (yet)");
+  };
+
   return (
     <div className={styles.card}>
       <p className={styles.title}>{data.title}</p>
       <img src={data.image} />
       <p className={styles.price}>${data.price.toFixed(2)}</p>
-      <p>Placeholder for Add to Cart stuff</p>
+      <form className={styles.cartForm}>
+        <div className={styles.formRow}>
+          <label htmlFor="qty">Qty</label>
+          <input
+            id="qty"
+            name="qty"
+            type="number"
+            value={quantity}
+            onChange={(e) => setQuantity(e.target.value)}
+          />
+        </div>
+        <div className={styles.ments}>
+          <button
+            className={styles.increment}
+            type="button"
+            onClick={increment}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <title>plus</title>
+              <path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
+            </svg>
+          </button>
+          <button
+            className={styles.decrement}
+            type="button"
+            onClick={decrement}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <title>minus</title>
+              <path d="M19,13H5V11H19V13Z" />
+            </svg>
+          </button>
+        </div>
+        <button className={styles.add} type="button" onClick={addToCart}>
+          Add to Cart
+        </button>
+      </form>
     </div>
   );
 }
