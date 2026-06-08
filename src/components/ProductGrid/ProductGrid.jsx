2c49a5b1953926fe5551dcbfb81702e23ad3cdb7
Tim Lupien <timlupien@gmail.com>
2026-06-08 17:11:28 -0500

    Overhaul styling
    It might not look fantastic or be written in a particularly clean or
    sensible way, but it *is* responsive now.

--- src/components/ProductGrid/ProductGrid.jsx
+++ src/components/ProductGrid/ProductGrid.jsx
@@ -1,12 +1,16 @@
 import ProductCard from "../ProductCard";
 import type1Styles from "./ProductGridType1.module.css";
 import type2Styles from "./ProductGridType2.module.css";
+import { useOutletContext } from "react-router";
 
 export default function ProductGrid({ styleType, products, isInCart }) {
   let styles = type1Styles;
   if (styleType == 2) {
     styles = type2Styles;
   }
+
+  const [cart, _] = useOutletContext();
+
   const productCards = products.map((p) => {
     return (
       <ProductCard
@@ -17,5 +21,35 @@ export default function ProductGrid({ styleType, products, isInCart }) {
       />
     );
   });
-  return <div className={styles.cardGrid}>{productCards}</div>;
+
+  const cartHeader = (
+    <>
+      <p className={styles.productTitle + " " + styles.tableLikeBracket}>
+        Product
+      </p>
+      <p className={styles.tableLikeBracket}>Price (Each)</p>
+      <p className={styles.tableLikeBracket}>Quantity</p>
+      <p className={styles.tableLikeBracket}>Total Price</p>
+    </>
+  );
+
+  const grandTotal = Object.values(products).reduce(
+    (p, c) => p + c.price * c.quantity,
+    0,
+  );
+
+  const totalDisplay = (
+    <div className={styles.grandTotalDisplay}>
+      {isInCart && <p className={styles.grandTotalLabel}>Grand Total</p>}
+      {isInCart && <p className={styles.grandTotalValue}>${grandTotal}</p>}
+    </div>
+  );
+
+  return (
+    <div className={styles.cardGrid}>
+      {isInCart && cartHeader}
+      {productCards}
+      {totalDisplay}
+    </div>
+  );
 }
